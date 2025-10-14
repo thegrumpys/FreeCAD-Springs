14046abdd3385332b9b9c26a32e921de04d21ac8
Brian D. Watt <bwatt@1fifoto.com>
2025-10-14 16:17:37 -0500

    Initial release

--- Features/Compression/Utils.py
+++ Features/Compression/Utils.py
@@ -25,7 +25,7 @@ def _as_float(value, default):
 
 def update_globals(obj) -> None:
     """Update global properties based on the object's global properties."""
-    if obj.PropCalcMethod == "Use values from material table":
+    if obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) == 1:
         obj.MaterialType = MUSIC_WIRE_MATERIAL_TYPE
         obj.ASTMFedSpec = MUSIC_WIRE_ASTM_FS + "/" + MUSIC_WIRE_FEDSPEC
         if obj.HotFactorKh < 1.0:
@@ -34,10 +34,76 @@ def update_globals(obj) -> None:
             obj.Process = "Cold Coiled"
         obj.Density = MUSIC_WIRE_DENSITY
         obj.TorsionModulus =  MUSIC_WIRE_SHEAR_MODULUS
-    elif obj.PropCalcMethod == "Use Tensile & %_Tensile_...":
+    elif obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) == 2:
         pass #tbd
-    else: # obj.PropCalcMethod == "Use Stress_Lim_...":
+    else: # obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) == 3
         pass #tbd
+
+#def cyclelife_calculation(material_type, life_category, spring_type, tensile, stress_at_deflection1, stress_at_deflection1) -> float:
+#    var i
+#    var j
+#    var pntc
+#    var sterm
+#    var temp
+#    var idxoffset
+#    var snx = [
+#    var sny = [7.0, 6.0, 5.0, 4.0 // Powers of 10: 10,000,000, 1,000,000, 100,000, 10,000 cycles
+#    var m_tab
+#    var result
+#    if (Material_File === "mat_metric.json") {
+#        m_tab = require('../mat_metric.json')
+#    } else {
+#        m_tab = require('../mat_us.json')
+#    }
+#    if (st_code === 3) {
+#        temp = tensile
+#    } else {
+#        temp = 0.67 * tensile
+#    }
+#    const smallnum = 1.0e-7
+#    var temp_stress_1 = temp - stress_1
+#    if (temp_stress_1 < smallnum) temp_stress_1 = smallnum
+#    var temp_stress_2 = temp - stress_2
+#    if (temp_stress_2 < smallnum) temp_stress_2 = smallnum
+#    var ratio = temp_stress_2 / temp_stress_1
+#    pntc = stress_2 - stress_1 * ratio
+#    if (pntc < smallnum) pntc = smallnum
+#    if (cl_idx < 5) { // Is Life Catagory Not Peened?
+#        j = 0
+#    } else { // Else Shot Peened
+#        j = 3
+#    }
+#    for (i = 0 i <= 3 i++) {
+#        idxoffset = 3 - i + j
+#        if (j > 0 && idxoffset === 3) { // If Shot Peened and
+#            idxoffset = 0
+#        }
+#        if (st_code === 3) { // Is it Torsion?
+#            snx[i = 0.01 * m_tab[mat_idx[mo.ptb1+idxoffset * tensile
+#        } else {
+#            snx[i = 0.01 * m_tab[mat_idx[mo.pte1+idxoffset * tensile
+#        }
+#    }
+#    if (pntc < snx[0) { // Is point after the table?
+#        sterm = (sny[1 - sny[0) / (snx[1 - snx[0)
+#        temp = sterm * (pntc - snx[0) + sny[0
+#        result =  Math.pow(10.0, temp)
+#        return(result)
+#    }
+#    // Look for the point in the table
+#    for (i = 1 i <= 3 i++) {
+#        if (pntc < snx[i) {
+#          j = i - 1
+#          sterm = (sny[i - sny[j) / (snx[i - snx[j)
+#          temp = sterm * (pntc - snx[j) + sny[j
+#          result = Math.pow(10.0, temp)
+#          return result
+#        }
+#    }
+#    sterm = (sny[3 - sny[2) / (snx[3 - snx[2)
+#    temp = sterm * (pntc - snx[3) + sny[3
+#    result =  Math.pow(10.0, temp)
+#    return result
 
 def update_properties(obj) -> None:
     """Update properties based on the object's properties."""
@@ -62,9 +128,42 @@ def update_properties(obj) -> None:
     obj.StressAtDeflection1 = s_f * obj.ForceAtDeflection1
     obj.StressAtDeflection2 = s_f * obj.ForceAtDeflection2
     obj.StressAtSolid = s_f * obj.ForceAtSolid
-    if obj.getEnumerations("PropCalcMethod").index(obj.PropCalcMethod) == 1:
+    if obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) == 1:
         obj.Tensile = obj.slope_term * (math.log10(obj.WireDiameter) - obj.const_term) + obj.tensile_010
-    elif obj.getEnumerations("PropCalcMethod").index(obj.PropCalcMethod) <= 2:
-        pass # tbd
+    elif obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) <= 2:
+        obj.StressLimitEndurance = obj.Tensile * obj.PercentTensileEndurance / 100.0
+        obj.StressLimitStatic  = obj.Tensile * obj.PercentTensileStatic  / 100.0
+    if obj.StressAtDeflection2 > 0.0:
+        obj.FactorOfSafetyAtDeflection2 = obj.StressLimitStatic / obj.StressAtDeflection2
+    else:
+        obj.FactorOfSafetyAtDeflection2 = 1.0
+    if obj.StressAtSolid > 0.0:
+        obj.FactorOfSafetyAtSolid = obj.StressLimitStatic / obj.StressAtSolid
+    else:
+        obj.FactorOfSafetyAtSolid = 1.0
+    stress_average = (obj.StressAtDeflection1 + obj.StressAtDeflection2) / 2.0
+    stress_range = (obj.StressAtDeflection2 - obj.StressAtDeflection1) / 2.0
+    se2 = obj.StressLimitEndurance / 2.0
+    obj.FactorOfSafetyAtCycleLife =  obj.StressLimitStatic / (kc * stress_range * (obj.StressLimitStatic - se2) / se2 + stress_average)
+    if obj.getEnumeration("PropCalcMethod").index(obj.PropCalcMethod) == 1 and obj.Material_Type != 0:
+#        obj.CycleLife = cyclelife_calculation(obj.MaterialType, obj.LifeCategory, 1, obj.Tensile, obj.StressAtDeflection1, obj.StressAtDeflection2)
+        obj.Cycle_Life = 0.0
+    else:
+        obj.Cycle_Life = 0.0
+    sq1 = obj.LengthAtFree
+    sq2 = obj.CoilsTotal * math.pi * obj.MeanDiameter
+    wire_len_t = math.sqrt(sq1 * sq1 + sq2 * sq2)
+    if obj.getEnumeration("EndType").index(obj.EndType) == 5:
+        wire_len_t = wire_len_t - 3.926 * obj.WireDiameter
+    obj.Weight = obj.Density * (math.pi * obj.WireDiameter * obj.WireDiameter / 4.0) * wire_len_t
+    if obj.LengthAtFree > obj.LengthAtSolid:
+        obj.PercentAvailableDeflection = 100.0 * obj.Deflection2 / (obj.LengthAtFree - obj.LengthAtSolid)
+        if obj.LengthAtFree < obj.LengthAtSolid + obj.WireDiameter:
+            temp = 100.0 * obj.Deflection2 / obj.WireDiameter + 10000.0 * (obj.LengthAtSolid + obj.WireDiameter - obj.LengthAtFree)
+            if temp < obj.PercentAvailableDeflection:
+                obj.PercentAvailableDeflection = temp
+    else:
+        obj.PercentAvailableDeflection = 100.0 * obj.Deflection2 / obj.WireDiameter + 10000.0 * (obj.LengthAtSolid + obj.WireDiameter - obj.LengthAtFree)
+    obj.Energy = 0.5 * obj.Rate * (obj.Deflection2 * obj.Deflection2 - obj.Deflection1 * obj.Deflection1)
 
     #=====================================
